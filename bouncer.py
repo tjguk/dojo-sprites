a82fc6d401f751f2b39ff1e0b5234af6f482a962
Tim Golden <mail@timgolden.me.uk>
2011-02-03 17:00:41 +0000

    Little more work on the bouncer

--- bouncer.py
+++ bouncer.py
@@ -5,10 +5,10 @@ import pygame
 from pygame.locals import *
 
 class Bouncer (pygame.sprite.DirtySprite):
-  def __init__ (self, image, topleft=(0,0)):
+  def __init__ (self, image):
     super (Bouncer, self).__init__ ()
     self.image = image
-    self.rect = self.image.get_rect (topleft=topleft)
+    self.rect = self.image.get_rect (topleft=(random.randint (0, 100), random.randint (0, 100)))
     self.speed = random.randint (1, 10)
     self.movement = [self.speed*random.choice ([-1, 0, 1]), self.speed*random.choice ([-1, 0, 1])]
   def update (self):
@@ -21,4 +21,11 @@ class Bouncer (pygame.sprite.DirtySprite):
       self.movement[1] = self.speed
     if self.rect.bottom > screenrect.bottom:
       self.movement[1] = -self.speed
+
+    for group in self.groups ():
+      for sprite in pygame.sprite.groupcollide (self, group, False):
+        if sprite != self:
+          self.movement = [m*-1 for m in self.movement]
+          break
+
     self.dirty = 1
